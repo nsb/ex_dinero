2dea7f2c564f23b44bd7beba68994cb165db54b3
Niels Sandholt Busch <niels.busch@maersk.com>
2019-04-24 10:07:04 +0200

    add name

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule ExDinero.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      name: "ExDinero",
       description: "Elixir Api client for danish accounting service Dinero.",
       source_url: "https://github.com/nsb/ex_dinero"
     ]
